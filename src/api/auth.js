124671d52b1be238cdb4cef4aebb251760fe480c
Keely <hellennnnn1@gmail.com>
2023-08-18 21:23:14 +0800

    Add API

---
+++ src/api/auth.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+const authURL = 'https://todo-list.alphacamp.io/api/auth';
+
+export const login = async ({ username, password }) => {
+  try {
+    const { data } = await axios.post(`${authURL}/login`, {
+      username,
+      password,
+    });
+    const { authToken } = data;
+    if (authToken) {
+      return { success: true, ...data };
+    }
+    return data;
+  } catch (error) {
+    console.error('[Login Failed]:', error);
+  }
+};
+
+export const register = async ({ username, email, password }) => {
+  try {
+    const { data } = await axios.post(`${authURL}/register`, {
+      username,
+      email,
+      password,
+    });
+
+    const { authToken } = data;
+
+    if (authToken) {
+      return { success: true, ...data };
+    }
+    return data;
+  } catch (error) {
+    console.error('[register Failed]:', error);
+  }
+};
+
+export const checkPermission = async (authToken) => {
+  try {
+    const response = await axios.get(`${authURL}/test-token`, {
+      headers: {
+        //依據 API 規格傳入 authToken， Bearer規定後面要保留一個空格
+        Authorization: 'Bearer ' + authToken,
+      },
+    });
+    return response.data.success;
+  } catch (error) {
+    console.error('[Check Permission Failed]:', error);
+  }
+};
